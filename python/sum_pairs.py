44992cd5c1ea49b06f3dba03cfd3a4f63b780218
robertuptc <robertuptc@gmail.com>
2025-03-06 15:24:18 -0500

    python problem finished

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,11 @@
-def sum_pairs():
+def sum_pairs(num_list, num):
+    seen = set()
+    answer = []
+
+    for n in num_list:
+        complement = num - n
+        if complement in seen:
+            answer.append([complement, n])
+        seen.add(n)
+        
+    return answer if len(answer) > 0 else 'unable to find pairs'
