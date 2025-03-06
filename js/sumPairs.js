ac7e398c8160192c95ba6c35c38b2c6e5d0ae850
robertuptc <robertuptc@gmail.com>
2025-03-06 12:06:23 -0500

    js problem finished

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,30 @@
-exports.sumPairs = function() {
+// function sumPairs(arr, num) {
+//     // Create a vari able ANSWER that equals to an empty array
+//     let answer = [];
 
-};
+//     // Do a 2 dimensional loop, if LOOP_ONE[i] + LOOP_TWO[j = i + 1] == NUM, append [i_ONE, i_TWO] to the ANSWER
+//     for(let i = 0; i < arr.length - 1; i++) {
+//         for(let j = i + 1; j < arr.length; j++) {
+//             if (arr[i] + arr[j] === num) {
+//                 answer.push([arr[i], arr[j]])
+//             }
+//         }
+//     }
+//     return answer.length > 0 ? answer : 'unable to find pairs'
+// };
+
+function sumPairs(arr, num) {
+    let seen = new Set();
+    let answer = [];
+
+    for(i = 0; i < arr.length; i++) {
+        let complement = num - arr[i];
+        if (seen.has(complement)) {
+            answer.push([complement, arr[i]]);
+        }
+        seen.add(arr[i])
+    }
+    return answer.length > 0 ? answer : 'unable to find pairs'
+}
+
+module.exports = {sumPairs}
--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -1,3 +1,6 @@
 var sp = require("./sumPairs");
 
 // Don't forget to add your tests :)
+console.log(sp.sumPairs([1,2,3,4,5], 9))
+console.log(sp.sumPairs([1,2,3,4,5], 7))
+console.log(sp.sumPairs([3, 1, 5, 8, 2], 27))
